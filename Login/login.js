3bedc1af6cf86f0add42a822b224d3387d1ceb4f
BeJuanitox <jsebastian23@hotmail.com>
2022-11-25 22:18:00 -0500

    Add login

---
+++ Login/login.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+  $(`#log`).click(function () {
+
+    window.open("#", "_self");
+
+  })
+
+  $(`#registrarse`).click(function () {
+
+    window.open("/Registro/registro.html", "_self");
+
+  })
+
+})
